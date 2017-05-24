54f041bcf7c5f7817cbb3b7601c6b632c61a0b5e
Takada <tkdtkd0220@gmail.com>
2017-05-24 23:15:23 +0900

    add finish and result action to questionnaires

--- app/controllers/questionnaires_controller.rb
+++ app/controllers/questionnaires_controller.rb
@@ -61,6 +61,12 @@ class QuestionnairesController < ApplicationController
     end
   end
 
+  def finish
+  end
+
+  def result
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_questionnaire
