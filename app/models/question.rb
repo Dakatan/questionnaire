c09cc7685e63a33d3531dae97cf444e467b4c289
Takada <tkdtkd0220@gmail.com>
2017-05-29 00:11:04 +0900

    fix codeclimate

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,10 +3,10 @@ class Question < ApplicationRecord
   has_many :answers
 
   def prev
-    @prev ||= Question.find_by(questionnaire_id: self.questionnaire_id, no: self.no - 1)
+    @prev ||= Question.find_by(questionnaire_id: questionnaire_id, no: no.to_i - 1)
   end
 
   def next
-    @prev ||= Question.find_by(questionnaire_id: self.questionnaire_id, no: self.no + 1)
+    @prev ||= Question.find_by(questionnaire_id: questionnaire_id, no: no.to_i + 1)
   end
 end
