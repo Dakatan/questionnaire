73ae5bd98ac7251ba2a0480589c785c4371642a9
Takada <tkdtkd0220@gmail.com>
2017-05-29 00:01:35 +0900

    add prev and next to question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,12 @@
 class Question < ApplicationRecord
   belongs_to :questionnaire
   has_many :answers
+
+  def prev
+    @prev ||= Question.find_by(questionnaire_id: self.questionnaire_id, no: self.no - 1)
+  end
+
+  def next
+    @prev ||= Question.find_by(questionnaire_id: self.questionnaire_id, no: self.no + 1)
+  end
 end
