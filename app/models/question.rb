2801249aa29dc40fbba452da1ae64a4894b6ee8d
Takada <tkdtkd0022gmail.com>
2017-12-29 21:32:44 +0900

    add validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,10 @@ class Question < ApplicationRecord
   belongs_to :questionnaire
   has_many :answers
 
+  validates :no, presence: true, numericality: true
+  validates :title, presence: true
+  validates :question_type, presence: true
+
   def prev
     @prev ||= Question.find_by(questionnaire_id: questionnaire_id, no: no.to_i - 1)
   end
--- app/models/questionnaire.rb
+++ app/models/questionnaire.rb
@@ -1,4 +1,8 @@
 class Questionnaire < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :respondents, dependent: :destroy
+
+  validates :title, presence: true
+  validates :start_date, presence: true, format: /\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])*/
+  validates :end_date, presence: true, format: /\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])*/
 end
