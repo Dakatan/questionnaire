80f8a8f40cac4ba5953ee5ed59b2c3fc9850dc27
Takada <tkdtkd0022gmail.com>
2017-12-30 11:26:28 +0900

    remove field error

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,10 @@ module Darjeeling
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # No field with errors.
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      %Q(#{html_tag}).html_safe
+    end
   end
 end
