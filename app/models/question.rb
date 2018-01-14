9de7c277834d94390c3090e2bc112fd82ef527ab
Takada <tkdtkd0022gmail.com>
2018-01-14 17:59:28 +0900

    change question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,11 +11,11 @@ class Question < ApplicationRecord
   validate :json_array_lint
 
   def prev
-    @prev ||= Question.find_by(questionnaire_id: questionnaire_id, no: no.to_i - 1)
+    Question.find_by(questionnaire_id: questionnaire_id, no: no.to_i - 1)
   end
 
   def next
-    @prev ||= Question.find_by(questionnaire_id: questionnaire_id, no: no.to_i + 1)
+    Question.find_by(questionnaire_id: questionnaire_id, no: no.to_i + 1)
   end
 
   private
