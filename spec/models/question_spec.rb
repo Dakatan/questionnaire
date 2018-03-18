dd1795b89afbc2a6030213717cc46fe0fdb6f7cb
Takada <tkdtkd0022gmail.com>
2018-03-18 17:40:08 +0900

    add unit test

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -8,7 +8,7 @@ describe Question do
 
     it 'question has prev' do
       prev_question = create(:question)
-      question = create(:question, id: 2, no: 2)
+      question = create(:question, id: 2)
       expect(question.prev).to eq prev_question
     end
 
@@ -25,7 +25,7 @@ describe Question do
 
     it 'question has next' do
       question = create(:question)
-      next_question = create(:question, id: 2, no: 2)
+      next_question = create(:question, id: 2)
       expect(question.next).to eq next_question
     end
 
@@ -65,4 +65,101 @@ describe Question do
       expect(questionnaire).to eq @questionnaire
     end
   end
+
+  describe '#final?' do
+    before do
+      @questionnaire = create(:questionnaire)
+    end
+
+    it 'question is final' do
+      create(:question)
+      question = create(:question, id: 2)
+      expect(question.final?).to eq true
+    end
+
+    it 'question is not false' do
+      create(:question)
+      question = create(:question, id: 2)
+      expect(question.first?).to eq false
+    end
+  end
+
+  describe '#first?' do
+    before do
+      @questionnaire = create(:questionnaire)
+    end
+
+    it 'question is final' do
+      question = create(:question)
+      create(:question, id: 2, no: 2)
+      expect(question.first?).to eq true
+    end
+
+    it 'question is not false' do
+      create(:question)
+      question = create(:question, id: 2)
+      expect(question.first?).to eq false
+    end
+  end
+
+  describe '#destroy' do
+    before do
+      @questionnaire = create(:questionnaire)
+    end
+
+    it 'question destroy with remove organization questions' do
+      question1 = create(:question)
+      question2 = create(:question, id: 2)
+      question3 = create(:question, id: 3)
+      question4 = create(:question, id: 4)
+
+      expect(question1.no).to eq 1
+      expect(question2.no).to eq 2
+      expect(question3.no).to eq 3
+      expect(question4.no).to eq 4
+
+      question2.destroy
+
+      question1.reload
+      question3.reload
+      question4.reload
+
+      expect(question1.no).to eq 1
+      expect(question3.no).to eq 2
+      expect(question4.no).to eq 3
+    end
+  end
+
+  describe '#json_array_lint' do
+    before do
+      @questionnaire = create(:questionnaire)
+    end
+
+    it 'json format error' do
+      question = Question.new
+      question.id = 1
+      question.title = 'Title'
+      question.question_type = '01'
+      question.no = 1
+      question.json_text = 'TEST'
+
+      question.valid?
+
+      expect(question.errors.messages[:json_text]).not_to be_empty
+    end
+
+    it 'json array format error' do
+      question = Question.new
+      question.id = 1
+      question.title = 'Title'
+      question.question_type = '01'
+      question.no = 1
+      question.json_text = '{"TEST": "TEST"}'
+
+      question.valid?
+
+      expect(question.errors.messages[:json_text]).not_to be_empty
+    end
+  end
+
 end
